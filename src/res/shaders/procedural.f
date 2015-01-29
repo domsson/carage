55d2322ae436288c0f3ff20cf5b98d52c544378d
robertkoerber <s0545073@htw-berlin.de>
2015-01-29 18:02:18 +0100

    blackOrWhiteFix

--- src/res/shaders/procedural.f
+++ src/res/shaders/procedural.f
@@ -7,19 +7,16 @@ in vec3 pass_Normal;
 in vec3 pass_LightPosition;
 
 uniform sampler2D tex;
-uniform int viewportResolution[2];
 uniform int viewportWidth;
 uniform int viewportHeight;
 
 out vec4 out_Color;
 
 vec3 blackOrWhite() {
-	vec3 pixelColor = vec3(1, 0, 0);
-/*	if (int(pass_TextureCoord.s * viewportResolution[0] / 5) % 2 == 1) {
-		pixelColor = vec3(1, 1, 1);
-	}*/
-	if (viewportWidth == 800)
+	vec3 pixelColor = vec3(0, 0, 1);
+	if (int(pass_TextureCoord.s * viewportWidth / 5) % 2 == 1) {
 		pixelColor = vec3(0, 1, 0);
+	}
 	return pixelColor;
 }
 
