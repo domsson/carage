f72ce8d04944bbf7b629a5f3bcd13273e7091709
robertkoerber <s0545073@htw-berlin.de>
2015-01-29 19:14:11 +0100

    scanlines

--- src/res/shaders/procedural.f
+++ src/res/shaders/procedural.f
@@ -11,19 +11,23 @@ uniform int viewportWidth;
 uniform int viewportHeight;
 uniform int shaderTimer;
 
+const vec3 greenLight = vec3(0.27, 0.8, 0.27);
+const vec3 greenIntermediate = vec3(0.1, 0.7, 0.1);
+const vec3 greenDark = vec3(0, 0.68, 0)'
+
 out vec4 out_Color;
 
-vec3 blackOrWhite() {
-	vec3 pixelColor = vec3(0.27, 0.8, 0.27);
-	if (int(pass_TextureCoord.t * viewportHeight / 6) % 2 == 1) {
-		pixelColor = vec3(0, 0.68, 0);
+vec3 scanlines() {
+	vec3 pixelColor = greenLight;
+	if (int(pass_TextureCoord.t * viewportHeight) % 5 == 2 || int(pass_TextureCoord.t * viewportHeight) % 5 == 1) {
+		pixelColor = greenIntermediate;
 	}
-	if (int(pass_TextureCoord.t * viewportHeight) % 6 == 0 || int(pass_TextureCoord.t * viewportHeight) % 6 == 0) {
-		pixelColor = vec3(0.1, 0.7, 0.1);
+	if (int(pass_TextureCoord.t * viewportHeight) % 5 == 3 || int(pass_TextureCoord.t * viewportHeight) % 5 == 4) {
+		pixelColor = greenDark;
 	}
 	return pixelColor;
 }
 
 void main(void) {
-	out_Color = vec4(blackOrWhite(), 0.5);
+	out_Color = vec4(scanlines(), 0.5);
 }
