ead5ff0e668bf423d1037e0c3c9774663b8b6cbb
robertkoerber <s0545073@htw-berlin.de>
2015-01-29 19:45:28 +0100

    scanlines fix

--- src/res/shaders/procedural.f
+++ src/res/shaders/procedural.f
@@ -9,7 +9,7 @@ in vec3 pass_LightPosition;
 uniform sampler2D tex;
 uniform int viewportWidth;
 uniform int viewportHeight;
-uniform float shaderTimer;
+uniform float scanlineTimer;
 
 const vec3 greenLight = vec3(0.27, 0.8, 0.27);
 const vec3 greenIntermediate = vec3(0.1, 0.7, 0.1);
@@ -18,12 +18,12 @@ const vec3 greenDark = vec3(0, 0.68, 0);
 out vec4 out_Color;
 
 vec3 scanlines() {
-	int time = int(shaderTimer) % 6;
+	int time = int(scanlineTimer) % 6;
 	vec3 pixelColor = greenLight;
-	if (int(pass_TextureCoord.t * viewportHeight) % 6 == (0 + time % 6) || int(pass_TextureCoord.t * viewportHeight) % 6 == (3 + time % 6)) {
+	if (int(pass_TextureCoord.t * viewportHeight) % 6 == ((0 + time) % 6) || int(pass_TextureCoord.t * viewportHeight) % 6 == ((3 + time) % 6)) {
 		pixelColor = greenIntermediate;
 	}
-	if (int(pass_TextureCoord.t * viewportHeight) % 6 == (4 + time % 6) || int(pass_TextureCoord.t * viewportHeight) % 6 == (5 + time % 6)) {
+	if (int(pass_TextureCoord.t * viewportHeight) % 6 == ((4 + time) % 6) || int(pass_TextureCoord.t * viewportHeight) % 6 == ((5 + time) % 6)) {
 		pixelColor = greenDark;
 	}
 	return pixelColor;
